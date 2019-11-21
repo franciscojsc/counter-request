3854b100515aefb6c293360f4eba23c0761b00b3
Francisco Chaves <francisco.jschaves@gmail.com>
2019-11-21 16:30:04 -0300

    Mostrando o nome do servidor
    Recuperando o nome da máquina onde o Node.js está em execução

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const os = require("os");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
 const counter = { value: 0 };
+const server = { value: os.hostname };
 
 app.get("/", (req, res) => {
   counter.value++;
-  res.render("home", { counter });
+  res.render("home", { counter, server });
 });
 
 app.listen(8080);
